5d2a0b9c0aae67282ed9c0b2ecefcfa2ffa73f1c
Frédéric Cabestre <frederic.cabestre@easymile.com>
2018-02-06 15:01:23 +0100

    More refactoring
    * Avoiding global variables
    * Better reconciliation controlling logic

--- main.go
+++ main.go
@@ -16,40 +16,54 @@ import (
 )
 
 const kubeConfig = ".kube/config"
-const annotationKey = "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"
+const lbCertificateAnnotationKey = "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"
+const controllerAnnotationKey = "easymile.com/certificate-controller.class"
 
-var clientset *kubernetes.Clientset
-var controller cache.Controller
-var certificateArn string
-var store cache.Store
+type context struct {
+	controllerClass string
+	certificateArn  string
+	clientSet       *kubernetes.Clientset
+	store           cache.Store
+}
 
 func main() {
-	certificateArn = os.Getenv("CERTIFICATE_ARN")
+	certificateArn := os.Getenv("CERTIFICATE_CONTROLLER_CERT_ARN")
 	if certificateArn == "" {
 		logrus.Error("Undefined certificate ARN. Please set environment variable CERTIFICATE_ARN")
 		os.Exit(1)
 	}
+	controllerClass := os.Getenv("CERTIFICATE_CONTROLLER_CLASS")
+	if controllerClass == "" {
+		logrus.WithFields(logrus.Fields{"owner-id": controllerClass}).Error("Undefined owner identifier. Using default")
+		controllerClass = "default-owner"
+	}
 	logrus.WithFields(logrus.Fields{"arn": certificateArn}).Info("Certificate ARN found")
 	var err error
-	clientset, err = getClient()
+	clientSet, err := getClient()
 	if err != nil {
 		logrus.Error(err)
 		os.Exit(1)
 	}
-	watchServices()
+	context := context{
+		certificateArn:  certificateArn,
+		controllerClass: controllerClass,
+		clientSet:       clientSet,
+	}
+	watchServices(context)
 }
 
-func watchServices() {
-	watchList := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "services", "",
+func watchServices(context context) {
+	var controller cache.Controller
+	watchList := cache.NewListWatchFromClient(context.clientSet.CoreV1().RESTClient(), "services", "",
 		fields.Everything())
-	store, controller = cache.NewInformer(
+	context.store, controller = cache.NewInformer(
 		watchList,
 		&core.Service{},
 		time.Second*2,
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    handleServiceAdd,
-			UpdateFunc: handleServiceUpdate,
-			DeleteFunc: handleServiceDelete,
+			AddFunc:    context.handleServiceAdd,
+			UpdateFunc: context.handleServiceUpdate,
+			DeleteFunc: context.handleServiceDelete,
 		},
 	)
 
@@ -58,65 +72,85 @@ func watchServices() {
 	<-stop
 }
 
-func handleServiceAdd(obj interface{}) {
+func (context *context) handleServiceAdd(obj interface{}) {
 	service := obj.(*core.Service)
-	if service.Annotations["easymile.com/certificate"] != "" {
-		logrus.WithFields(logrus.Fields{"namespace": service.Namespace, "service": service.Name}).Info("Service added. Adding LoadBalancer annotation")
-		updateAnnotation(service, certificateArn)
-	}
+	context.reconcile(nil, service)
 }
-func handleServiceDelete(obj interface{}) {
+func (context *context) handleServiceDelete(obj interface{}) {
 	service := obj.(*core.Service)
-	logrus.WithFields(logrus.Fields{"namespace": service.Namespace, "service": service.Name}).Info("Service deleted. Removing LoadBalancer annotation")
+	context.reconcile(service, nil)
 }
 
-func handleServiceUpdate(old, current interface{}) {
-	// Cache access example
-	serviceInterface, exists, err := store.Get(current)
-	if exists && err == nil {
-		logrus.WithFields(logrus.Fields{"service": serviceInterface}).Debug("Service found in cache")
-	} else {
-		logrus.WithFields(logrus.Fields{"err": err}).Warn("Failed to service in cache")
-	}
-
-	service := current.(*core.Service)
+func (context *context) handleServiceUpdate(old, current interface{}) {
 	oldService := old.(*core.Service)
-	if service.Annotations["easymile.com/certificate"] != oldService.Annotations["easymile.com/certificate"] {
-		if service.Annotations["easymile.com/certificate"] == "true" {
-			logrus.WithFields(logrus.Fields{"namespace": service.Namespace, "service": service.Name}).Info("Adding LoadBalancer annotation")
-			updateAnnotation(service, certificateArn)
-		} else {
-			logrus.WithFields(logrus.Fields{"namespace": service.Namespace, "service": service.Name}).Info("Removing LoadBalancer annotation")
-			updateAnnotation(service, "")
-		}
+	newService := current.(*core.Service)
+	if isReconcileNeeded(newService, oldService) {
+		context.reconcile(oldService, newService)
 	}
 }
-
-func updateAnnotation(service *core.Service, annotationValue string) {
-	service.Annotations[annotationKey] = annotationValue
-	_, err := clientset.CoreV1().Services(service.Namespace).Update(service)
+func (context *context) reconcile(old *core.Service, new *core.Service) {
+	if new == nil {
+		if old != nil {
+			logrus.WithFields(logrus.Fields{"action": "none", "namespace": old.Namespace, "new": old.Name}).Info("Service deleted")
+		} else {
+			logrus.WithFields(logrus.Fields{"action": "unknown"}).Warn("Unexpected call")
+		}
+	} else if context.hasControllingAnnotation(new) {
+		if new.Annotations[lbCertificateAnnotationKey] == context.certificateArn {
+			logrus.WithFields(logrus.Fields{"action": "none", "namespace": new.Namespace, "new": new.Name}).Info("No action needed")
+		} else if new.Annotations[lbCertificateAnnotationKey] == "" {
+			logrus.WithFields(logrus.Fields{"action": "add", "namespace": new.Namespace, "new": new.Name}).Info("Adding LoadBalancer annotation")
+			context.updateAnnotation(new, context.certificateArn)
+		} else {
+			logrus.WithFields(logrus.Fields{"action": "update", "namespace": new.Namespace, "new": new.Name}).Info("Updating LoadBalancer annotation")
+			context.updateAnnotation(new, context.certificateArn)
+		}
+	} else if old != nil {
+		if context.hasControllingAnnotation(old) {
+			logrus.WithFields(logrus.Fields{"action": "delete", "namespace": old.Namespace, "new": old.Name}).Info("Deleting LoadBalancer annotation")
+			context.updateAnnotation(new, "")
+		}
+	} else {
+		logrus.WithFields(logrus.Fields{"action": "skip", "namespace": new.Namespace, "new": new.Name}).Info("Controller class does not match. Skipping")
+	}
+}
+func (context *context) updateAnnotation(service *core.Service, annotationValue string) {
+	service.Annotations[lbCertificateAnnotationKey] = annotationValue
+	_, err := context.clientSet.CoreV1().Services(service.Namespace).Update(service)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{"err": err}).Warn("Failed to update the service")
 	}
 }
 
+func (context *context) hasControllingAnnotation(new *core.Service) bool {
+	return new.Annotations[controllerAnnotationKey] == context.controllerClass
+}
+
 func getClient() (*kubernetes.Clientset, error) {
 	var config *rest.Config
 
-	KubeConfigPath := filepath.Join(os.Getenv("HOME"), kubeConfig)
+	kubeConfigPath := filepath.Join(os.Getenv("HOME"), kubeConfig)
 
-	if _, err := os.Stat(KubeConfigPath); os.IsNotExist(err) {
+	if _, err := os.Stat(kubeConfigPath); os.IsNotExist(err) {
 		logrus.Info("Using in cluster config")
 		config, err = rest.InClusterConfig()
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		logrus.WithFields(logrus.Fields{"config": KubeConfigPath}).Info("Using out of cluster config")
-		config, err = clientcmd.BuildConfigFromFlags("", KubeConfigPath)
+		logrus.WithFields(logrus.Fields{"config": kubeConfigPath}).Info("Using out of cluster config")
+		config, err = clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 		if err != nil {
 			return nil, err
 		}
 	}
 	return kubernetes.NewForConfig(config)
+}
+
+func isReconcileNeeded(newService *core.Service, oldService *core.Service) bool {
+	return hasAnnotationChanged(lbCertificateAnnotationKey, newService, oldService) ||
+		hasAnnotationChanged(controllerAnnotationKey, newService, oldService)
+}
+func hasAnnotationChanged(annotationKey string, newService *core.Service, oldService *core.Service) bool {
+	return newService.Annotations[annotationKey] != oldService.Annotations[annotationKey]
 }
